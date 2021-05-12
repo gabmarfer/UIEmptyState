6325c6cd454b3a1f9db1f7f39964752f85356002
gabmarfer <gabriel.martelo@gmail.com>
2021-05-12 12:51:21 +0200

    Add Package manifest.

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "UIEmptyState",
+    products: [
+       .library(name: "UIEmptyState", targets: ["UIEmptyState"]),
+    ],
+    dependencies : [
+    ],
+    targets: [
+       .target(name: "UIEmptyState", dependencies: [], path: "src"),
+    ]
+)
